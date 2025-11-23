0d36d3c0dcaaa697002c66617f71d706671458c0
Joel Ohikhena <joelohikhena12@gmail.com>
2025-11-23 22:15:48 +0100

    Quote component file path changed in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import NavBar from "./NavBar/NavBar.jsx";
 import Hero from "./Hero/Hero.jsx";
-import Quote from "./Quote/quote.jsx";
+import Quote from "./Quote/Quote.jsx";
 import Visiting from "./Visiting/Visiting.jsx";
 import Groups from "./Groups/Groups.jsx";
 import Attendance from "./Attendance/Attendance.jsx";
